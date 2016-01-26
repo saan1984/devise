fcaeb6912306a333102074fd18042d07eaf3858c
saan1984 <sandeep_giet@yahoo.com>
2016-01-27 00:27:26 +0530

    changes

---
+++ devise.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+var commander = require("commander"),
+    os = require("os");
+
+var getMachineName = function(){
+    console.log("Host name ",os.hostname());
+    return os.hostname();
+};
+
+var getPlatformType = function(){
+    console.log("Platform type ",os.hostname());
+    return os.platform();
+};
+
+commander.command('devise')
+    .description('returns the details of the host machine.')
+    .option("machineName", getMachineName)
+    .option("platformType", getPlatformType)
+    .action(function(){
+        return {
+            hostName:getMachineName(),
+            platformType:getPlatformType()
+        }
+    });
